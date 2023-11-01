fa293db48db8f8d1a7f5d9665f7b98453d0d1ee9
Tracy Xing <tataxing123@gmail.com>
2023-11-01 11:18:17 -0400

    Tuned Weight regularization
    Finished Tuning the Weight Regularization Updated it in README

--- README.md
+++ README.md
@@ -16,3 +16,26 @@ Basic Sign Language Translator that interprets ASL language using Machine Learni
 
 # Usage
 Run [pytorch_version.py](https://github.com/avyan-k/MAIS-202-F2023-ASL-Processing/blob/main/src/pytorch_version.py) to load the data. Training the model will be implemented at a later time.
+
+# Split Dataset
+With get_and_split_dataset() we split the dataset into training set (67.5%) validation set (22.5%) and testing set (10%)
+
+# Tuning Weight Regularization
+With tune_mean_std() we get the average weight for the mean and standard deviation for the each datasets and we get:
+
+- training dataset:
+    - mean:[0.4916, 0.4699, 0.4255]
+    - standard deviation:[0.1582, 0.1646, 0.1767]
+
+- validation dataset:
+    - mean:[0.4921, 0.4698, 0.4243]
+    - standard deviation: [0.1588, 0.1651, 0.1769]
+
+- testing dataset:
+    - mean:[0.4904, 0.4686, 0.4246]
+    - standard deviation:[0.1587, 0.1656, 0.1776]
+
+We can approximate the best weight by getting the average of weights of all datasets and we get:
+
+- mean: [0.4916,0.4697,0.4251]
+- standard deviation: [0.1584,0.1648,0.1768]
--- src/loading_dataset.py
+++ src/loading_dataset.py
@@ -1,26 +1,71 @@
 import os
 import torch
 from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, random_split
 import opendatasets as od
 import matplotlib.pyplot as plt
+import numpy as np
 
 PATH_TO_DATA = r"synthetic-asl-alphabet"
+test_data_path = PATH_TO_DATA + r"/Test_Alphabet"
+train_data_path = PATH_TO_DATA + r"/Train_Alphabet"
 TRAIN_SET_SIZE=24300
 TESTING_SET_SIZE=2700
 
 def download_data():
+    
     if not os.path.exists(r"synthetic-asl-alphabet"):
         od.download("https://www.kaggle.com/datasets/lexset/synthetic-asl-alphabet/data")
         
-def load_data():
-    download_data()
-    # Define the data paths
-    test_data_path = PATH_TO_DATA + r"/Test_Alphabet"
-    train_data_path = PATH_TO_DATA + r"/Train_Alphabet"
+def get_and_split_dataset():
     
+    # Loading The Dataset
+    full_train_dataset = datasets.ImageFolder(train_data_path,transforms.ToTensor())
+    test_dataset = datasets.ImageFolder(test_data_path,transforms.ToTensor())
+    
+    # Split the datasets into training, validation, and test sets
+    train_dataset, valid_dataset, _ = random_split(full_train_dataset, [int(TRAIN_SET_SIZE*0.75), int(TRAIN_SET_SIZE*0.25),0])
+    test_dataset, _ , _ = random_split(test_dataset, [TESTING_SET_SIZE, len(test_dataset) - TESTING_SET_SIZE, 0])
+    
+    return train_dataset,valid_dataset,test_dataset
+
+def tune_mean_std(train_dataset,valid_dataset,test_dataset):
+
+    # Create a DataLoader for the subset
+    train_loader = DataLoader(train_dataset, batch_size=100, shuffle=True)
+    valid_loader = DataLoader(valid_dataset, batch_size=100, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=100, shuffle=False)
+
+    # Calculate mean and standard deviation
+    means_stds = []
+    loaders = [train_loader,valid_loader,test_loader]
+    
+    for curr_loader in loaders:
+        
+        mean = torch.zeros(3)
+        std = torch.zeros(3)
+        
+        for data, _ in curr_loader: 
+            
+            data = data.view(data.size(0), data.size(1), -1)  # Flatten the images
+            mean += data.mean(2).sum(0)
+            std += data.std(2).sum(0)
+            
+        mean /= len(curr_loader.dataset)
+        std /= len(curr_loader.dataset)
+        
+        print("Mean:", mean)
+        print("Standard Deviation:", std)
+        
+        means_stds.append([mean,std])
+
+    return means_stds
+        
+def load_data():
+
     # Data Normalization
-    means = [0.5,0.5,0.5]
-    standard_deviations = [0.5,0.5,0.5]
+    means = [0.4916,0.4697,0.4251]
+    standard_deviations = [0.1584,0.1648,0.1768]
     
     # Data Augmentation
     randomizing_transforms = [transforms.RandomRotation(30),transforms.RandomHorizontalFlip()]
@@ -48,15 +93,17 @@ def load_data():
     return train_loader, valid_loader, test_loader
 
 def load_device():
+    
     if torch.backends.mps.is_built():
         device = torch.device("mps")
-    elif torch.cuda.is_available():# If a GPU is available, use it
+    elif torch.cuda.is_available(): # If a GPU is available, use it
         device = torch.device("cuda")
     else: # Else, revert to the default (CPU)
         device = torch.device("cpu")
     return device
 
 if __name__ == "__main__":
+    
     download_data()
     train_loader, valid_loader, test_loader = load_data()
     
@@ -65,4 +112,4 @@ if __name__ == "__main__":
         plt.title(f"Label: {labels[0]}")
         plt.show()
         break
-   
+    
