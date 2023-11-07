3a9c0e5c5df343f9d48dcfb0d6647eec89b0a57b
avyan-k <avyan.kandasamy@gmail.com>
2023-11-07 10:53:47 -0500

    Mod .gitignore to not include results and cache Added batch normalization to CNN model

--- src/CNN_model.py
+++ src/CNN_model.py
@@ -24,6 +24,8 @@ class CNN_model(nn.Module):
     
     self.convolutional_network = nn.ModuleList() # list that will store the convolutional layers of the model
     kernelsPerLayers = initialKernels
+    #add batch normalization layer
+    self.convolutional_network.append(nn.BatchNorm2d(3))
     self.convolutional_network.append(nn.Conv2d(in_channels=3,out_channels=kernelsPerLayers, kernel_size=5,padding="same"))
     
     for index in range(numberConv-1):
@@ -33,19 +35,21 @@ class CNN_model(nn.Module):
       
 		# computes the flattened output size after convolutional and pooling layers
 		# 64
-    self.flatten = int((512 / (2**(numberConv if numberConv<3 else 3))))**2 * initialKernels * 2 **(numberConv-1)
+    self.flatten = int((512 / (2**(min(3,numberConv )))))**2 * initialKernels * 2 **(numberConv-1)
 		
 		# store dense (fully connected) layers, both hidden and final classification layers.
     self.dense_network = nn.ModuleList()
 
     if numberDense > 0:
-      
+      #add batch normalization layer
+      self.dense_network.append(nn.BatchNorm1d(self.flatten))
       # add the dense layers appropriately
       self.dense_network.append(nn.Linear(self.flatten, neuronsDLayer)) # first dense layer
 
       for i in range(numberDense - 1): # one dense layer has already been added
         self.dense_network.append(nn.Linear(neuronsDLayer, neuronsDLayer))
-
+        #add batch normalization layer
+        self.dense_network.append(nn.BatchNorm1d(neuronsDLayer))
       # classification layer - not counted as part of (hidden)dense network
       self.dense_network.append(nn.Linear(neuronsDLayer, 27))
 
@@ -71,8 +75,6 @@ class CNN_model(nn.Module):
 
 		# fully connected (dense) layers defined in self.dense_network
     for dense_layer in self.dense_network:
-      
-      x = dense_layer(x) # applies a linear transformation to the data
       x = F.leaky_relu(dense_layer(x)) # Leaky ReLU activation function
 
 		# converts the model's raw output into class probabilities
@@ -81,12 +83,12 @@ class CNN_model(nn.Module):
     return x # returns predicted class probabilities for each input
   
 
-  def train_model(cnn,train_loader, valid_loader, test_loader, num_epochs = 200,num_iterations_before_validation = 30,weight_decay=0.00001):
+  def train_model(self,train_loader, valid_loader, test_loader, num_epochs = 200,num_iterations_before_validation = 30,weight_decay=0.00001):
     
     start = time.time()
 
     # hyperparameters:
-    lr_values = {0.001, 0.01, 0.1}
+    lr_values = {0.1, 0.01, 0.001}
 
 		# to store metrics and models for different learning rates during training
     cnn_metrics = {}
@@ -107,7 +109,7 @@ class CNN_model(nn.Module):
 			# to set up for a multiclass classification task with 27 classes
       accuracy = torchmetrics.Accuracy(task="multiclass", num_classes=27).to(DEVICE) # Regular accuracy
 
-      cnn = cnn.to(DEVICE)
+      cnn = self.to(DEVICE)
 
 			# Initializes the Adam optimizer with the model's parameters
       optimizer = optim.Adam(cnn.parameters(), lr,weight_decay=weight_decay)
@@ -118,8 +120,7 @@ class CNN_model(nn.Module):
         # Iterate through the training data
         for iteration, (X_train, y_train) in enumerate(train_loader):
 
-          # gradients of model's parameters are zeroed to avoid accumulating gradients from previous iterations
-          optimizer.zero_grad()
+          
 
           X_train = X_train.to(DEVICE)
           y_train = y_train.to(DEVICE)
@@ -134,46 +135,49 @@ class CNN_model(nn.Module):
 					# takes a step in the direction that minimizes the loss
           optimizer.step()
           
+          # gradients of model's parameters are zeroed to avoid accumulating gradients from previous iterations
+          optimizer.zero_grad()
           # validation check
-          if iteration % num_iterations_before_validation == 0:
+          # if iteration % num_iterations_before_validation == 0:
             
-            # Disable gradient calculations
-            with torch.no_grad():
+          #   # Disable gradient calculations
+          #   with torch.no_grad():
 
-              val_accuracy_sum = 0
-              val_loss_sum = 0
+          #     val_accuracy_sum = 0
+          #     val_loss_sum = 0
 
-              for X_val, y_val in valid_loader:
+          #     for X_val, y_val in valid_loader:
 
-                X_val = X_val.to(DEVICE)
-                y_val = y_val.to(DEVICE)
+          #       X_val = X_val.to(DEVICE)
+          #       y_val = y_val.to(DEVICE)
 
-                y_hat = cnn(X_val)
+          #       y_hat = cnn(X_val)
                 
-                val_accuracy_sum += accuracy(y_hat, y_val)
-                val_loss_sum += loss(y_hat, y_val)
+          #       val_accuracy_sum += accuracy(y_hat, y_val)
+          #       val_loss_sum += loss(y_hat, y_val)
                 
-              # average validation accuracy and loss
-              val_accuracy = (val_accuracy_sum / len(valid_loader)).cpu()
-              val_loss = (val_loss_sum / len(valid_loader)).cpu()
+          #     # average validation accuracy and loss
+          #     val_accuracy = (val_accuracy_sum / len(valid_loader)).cpu()
+          #     val_loss = (val_loss_sum / len(valid_loader)).cpu()
 
-              cnn_metrics[lr]["accuracies"].append(val_accuracy)
-              cnn_metrics[lr]["losses"].append(val_loss)
+          #     cnn_metrics[lr]["accuracies"].append(val_accuracy)
+          #     cnn_metrics[lr]["losses"].append(val_loss)
 
-              # log validation metric
-              text_file = open("results\learning_rate_training.txt", "a") 
-              text_file.write(f"LR = {lr} --- EPOCH = {epoch} --- ITERATION = {iteration}\n")
-              text_file.write(f"Validation loss = {val_loss} --- Validation accuracy = {val_accuracy}\n")
-              text_file.write("It has now been "+ str(time.time() - start) +" seconds since the beginning of the program\n")
-              print("It has now been "+ time.strftime("%Mm%Ss", time.gmtime(time.time() - start))  +"  since the beginning of the program")
-              text_file.close() 
+          #     # log validation metric
+          #     text_file = open("results\learning_rate_training.txt", "a") 
+          #     text_file.write(f"LR = {lr} --- EPOCH = {epoch} --- ITERATION = {iteration}\n")
+          #     text_file.write(f"Validation loss = {val_loss} --- Validation accuracy = {val_accuracy}\n")
+          #     text_file.write("It has now been "+ str(time.time() - start) +" seconds since the beginning of the program\n")
+          #     print("It has now been "+ time.strftime("%Mm%Ss", time.gmtime(time.time() - start))  +"  since the beginning of the program")
+          #     text_file.close() 
         
         # log training metric
-        text_file = open("results\subset_training.txt", "a")  
-        print("loss:", train_loss.item(),"epoch:", epoch)
-        text_file.write("loss: "+ str(train_loss.item()) +" epoch: "+ str(epoch) + "\n")
-        print("It has now been", time.strftime("%Mm%Ss", time.gmtime(time.time() - start)),"since the beginning of the program")
-        text_file.write(time.strftime("%Mm%Ss", time.gmtime(time.time() - start))  +" since the beginning of the program\n")
+        text_file = open("results/training/subset_training.txt", "a")  
+        print(f"loss: {train_loss.item()} epoch: {epoch}")
+        text_file.write(f"loss: {train_loss.item()} epoch: {epoch}\n")
+        current =  time.strftime("%Mm%Ss", time.gmtime(time.time() - start))
+        print(f"It has now been {current} since the beginning of the program")
+        text_file.write(f"It has now been {current} since the beginning of the program\n")
         
     return cnn_metrics
           
@@ -190,7 +194,7 @@ def plot_parameter_testing(cnn_metrics,num_iterations_before_validation):
 
 if __name__ == "__main__":
   
-  cnn = CNN_model(numberDense=0, neuronsDLayer=20)
+  cnn = CNN_model(numberDense=10, neuronsDLayer=20)
   cnn_metrics = cnn.train_model(train_loader, valid_loader, test_loader)
   
   # plot_parameter_testing(cnn_metrics, 1000)
