7365bb9d6843423908003281d89b1d630c643e49
avyan-k <avyan.kandasamy@gmail.com>
2023-11-17 20:40:53 -0500

    Model maybe overfits small dataset, to be tweaked

--- src/CNN_model.py
+++ src/CNN_model.py
@@ -25,80 +25,101 @@ class CNN_model(nn.Module):
   def __init__(self,numberConvolutionLayers=3,initialKernels=5,numberDense = 0,neuronsDLayer=0,dropout=0.5,dataset = "ASL"):
     
     super(CNN_model, self).__init__() # calls the constructor of the parent class (nn.Module) to properly initialize the model
-    
-    self.convolutional_network = nn.ModuleList() # list that will store the convolutional layers of the model
-    kernelsPerLayers = initialKernels
+    self.network = nn.Sequential(
+      nn.Conv2d(in_channels=3,out_channels=64, kernel_size=3,padding="same"),
+      nn.BatchNorm2d(64),
+      nn.ReLU(),
+      nn.MaxPool2d(2),
+      
+      nn.Conv2d(in_channels=64,out_channels=128, kernel_size=3,padding="same"),
+      nn.BatchNorm2d(128),
+      nn.ReLU(),
+      nn.MaxPool2d(2),
 
+      nn.Conv2d(in_channels=128,out_channels=256, kernel_size=3,padding="same"),
+      nn.BatchNorm2d(256),
+      nn.ReLU(),
+      nn.MaxPool2d(2),
+
+      nn.Conv2d(in_channels=256,out_channels=512, kernel_size=3,padding="same"),
+      nn.BatchNorm2d(512),
+      nn.ReLU(),
+      nn.MaxPool2d(2),
+
+      nn.Flatten(start_dim=1),
+      nn.Dropout(p = 0.5),
+      nn.Linear(2048, 1024),
+      nn.BatchNorm1d(1024),
+      nn.ReLU(),
+      nn.Dropout(p = 0.5),
+      nn.Linear(1024, 27)
+
+
+    )
+    # Setting up databases constants
     if dataset == "ASL":
-      channels,classes,image_size = (3,27,(512,513))
+      channels,classes,image_size = (3,27,(32,32))
     elif dataset == "MNIST":
       channels,classes,image_size = (1,10,(28,28))
     else:
-      raise AssertionError("Unknown dataset", dataset)
-
-    # self.convolutional_network.append(nn.BatchNorm2d(3)) # TODO
-    self.convolutional_network.append(nn.Conv2d(in_channels=channels,out_channels=kernelsPerLayers, kernel_size=3,padding="same"))
+      raise AttributeError("Unknown dataset", dataset)
     
-    for index in range(numberConvolutionLayers-1):
-      
-      self.convolutional_network.append(nn.Conv2d(in_channels=kernelsPerLayers,out_channels=kernelsPerLayers*2, kernel_size=3,padding="same"))
-      kernelsPerLayers *= 2 # TODO SWITCH BACK
-      
-      
-		# computes the flattened output size after convolutional and pooling layers
-		# 64
-    # kernelsPerLayers = 0 # TODO remove, rest for MLP only
-    # self.flatten = int(((image_size[0]*image_size[1])/(2**(numberConvolutionLayers))) * kernelsPerLayers)
-    self.flatten = int((image_size[0] / (2**(numberConvolutionLayers ))))**2 * kernelsPerLayers
+    #Convolutional Network
+    self.convolutional_network = nn.ModuleList() # list that will store the convolutional layers of the model
+    kernelsPerLayers = initialKernels
 
-		# store dense (fully connected) layers, both hidden and final classification layers.
-    self.dense_network = nn.ModuleList()
-
-    if numberDense > 0:
-      #add batch normalization layer
-      #self.dense_network.append(nn.BatchNorm1d(self.flatten)) # TODO
-      # add the dense layers appropriately
-      self.dense_network.append(nn.Linear(self.flatten, neuronsDLayer)) # first dense layer
-
-      for i in range(numberDense - 1): # one dense layer has already been added
-        self.dense_network.append(nn.Linear(neuronsDLayer, neuronsDLayer))
-        #add batch normalization layer
-        # self.dense_network.append(nn.BatchNorm1d(neuronsDLayer)) # TODO
-      # classification layer - not counted as part of (hidden)dense network
-      self.dense_network.append(nn.Linear(neuronsDLayer, classes))
+    self.convolutional_network.append(nn.Conv2d(in_channels=channels,out_channels=kernelsPerLayers, kernel_size=3,padding="same")) #first convolutional layer
+    self.convolutional_network.append(nn.BatchNorm2d(kernelsPerLayers)) #batch normalize the data
+    for i in range(numberConvolutionLayers - 1):
+      self.convolutional_network.append(nn.Conv2d(in_channels=kernelsPerLayers,out_channels=kernelsPerLayers*2, kernel_size=3,padding="same")) #add convolution layer
+      kernelsPerLayers *= 2 #double kernels everytime
+      self.convolutional_network.append(nn.BatchNorm2d(kernelsPerLayers)) #batch normalize the data
 
 
-    else: # only 1 dense layer for classifications - no hidden - default
+    flattened = int((image_size[0] / (2**(numberConvolutionLayers))))**2 * kernelsPerLayers #Flattened Output of Convolutional Layers
+    self.dropout_layer = nn.Dropout(p=dropout) # dropout to reduce model and prevent overfitting
 
-      self.dense_network.append(nn.Linear(self.flatten, classes))
+    #Dense Network
+    self.dense_network = nn.ModuleList() # list that will store the dense layers of the model
+    neurons = neuronsDLayer
+
+    self.dense_network.append(nn.Linear(flattened, neurons)) # first dense layer
+    self.dense_network.append(nn.BatchNorm1d(neurons)) #batch normalize the data
+    for i in range(numberDense-1): # one dense layer has already been added
+      self.dense_network.append(nn.Linear(neurons, neurons))
+      self.convolutional_network.append(nn.BatchNorm1d(neurons)) #batch normalize the data
+
+    self.dense_network.append(nn.Linear(neurons, classes)) # classification layer - not counted as part of (hidden)dense network
+
 
   def forward(self, x):
     
     '''Forward pass function, needs to be defined for every model'''
-
     for index,convLayer in enumerate(self.convolutional_network):
-
       x = convLayer(x) # applies a convolution operation to the input
-      x = F.relu(x) # activation function
-      x = F.max_pool2d(x, 2) # 2x2 maxpool each convolutional layer except the last one
+      if index % 2 == 1: #note that every other layer is batch normalize, we only activate and max pool for convolution
+          x = F.relu(x) # activation function
+          x = F.max_pool2d(x, 2) # 2x2 maxpool each convolutional layer except the first one
 
 
     x = torch.flatten(x, start_dim = 1) # Flatten to a 1D vector
+    x = self.dropout_layer(x) # dropout on some of the convolution
 
 		# fully connected (dense) layers defined in self.dense_network
-    for i, dense_layer in enumerate(self.dense_network):
-      x = dense_layer(x)
-      if i < len(self.dense_network) - 1:
-        x = F.relu(x) # ReLU activation function
-
-		# converts the model's raw output into class probabilities
-    x = F.softmax(x, dim = 1) # dim = 1 to softmax along the rows of the output
+    for index, dense_layer in enumerate(self.dense_network):
+      if index == len(self.dense_network) - 1: #dropout on last layer
+        x = self.dropout_layer(x)
+        x = dense_layer(x)
+      else:
+        x = dense_layer(x)
+        if index % 2 == 1: #again only activate after batch normalizing
+          x = F.relu(x) # ReLU activation function
 
     return x # returns predicted class probabilities for each input
   
 
-def train_model(cnn,train_loader, valid_loader, test_loader, num_epochs = 200,num_iterations_before_validation = 30,weight_decay=0.00001):
-  cnn.train() #maybe putting the model in train mode was what we had to do??
+def train_model(cnn,train_loader, valid_loader, test_loader, num_epochs = 200,num_iterations_before_validation = 27,weight_decay=0.00001):
+  losses = np.empty(num_epochs)
   start = time.time()
   text_file = open(r"results\training\losses.txt", "w")
   text_file.write(f"Attempting {num_epochs} epochs on date of {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
@@ -110,8 +131,9 @@ def train_model(cnn,train_loader, valid_loader, test_loader, num_epochs = 200,nu
 
   # Initializes the Adam optimizer with the model's parameters
   # optimizer = optim.SGD(cnn.parameters(), lr,weight_decay=wd)
-  optimizer = optim.Adam(cnn.parameters(), lr=0.1,weight_decay=0.01)
+  optimizer = optim.Adam(cnn.parameters(), lr=0.001,weight_decay=0.01)
   loss = nn.CrossEntropyLoss().to(DEVICE)
+  accuracy = torchmetrics.Accuracy(task="multiclass", num_classes=27).to(DEVICE)
   # cnn_models[lr] = cnn
   # cnn_models[wd] = cnn
   
@@ -122,39 +144,85 @@ def train_model(cnn,train_loader, valid_loader, test_loader, num_epochs = 200,nu
 
       # print(y_train)
       optimizer.zero_grad()
-      
+
       X_train = X_train.to(DEVICE)
       y_train = y_train.to(DEVICE)
       
       # forward pass of the CNN model on the input data to get predictions
       y_hat = cnn(X_train)
       
-      # if iteration % 100 == 0:
-      #   print(f"Iteration: {iteration} Predictions:\n{y_hat}")
+
+
       optimizer.zero_grad()
       
       # comparing the model's predictions with the truth labels
       train_loss = loss(y_hat, y_train)
-
+      # if iteration % 9 == 0:
+      #   print(f"Epoch: {epoch} Iteration: {iteration} Loss: {train_loss}")
+      #   print(f"Predictions:\n{y_hat}")
       # backpropagating the loss through the model
       train_loss.backward()
 
       # takes a step in the direction that minimizes the loss
       optimizer.step()
 
+      if iteration % num_iterations_before_validation  == 0 and epoch > (num_epochs/2):
+        with torch.no_grad():
+
+          # Keep track of the losses & accuracies
+          val_accuracy_sum = 0
+          val_loss_sum = 0
+
+          # Make a predictions on the full validation set, batch by batch
+          for X_val, y_val in valid_loader:
+
+            # Move the batch to GPU if it's available
+            X_val = X_val.to(DEVICE)
+            y_val = y_val.to(DEVICE)
+
+            y_hat = cnn(X_val)
+            val_accuracy_sum += accuracy(y_hat, y_val)
+            val_loss_sum += loss(y_hat, y_val)
+
+          # Divide by the number of iterations (and move back to CPU)
+          val_accuracy = (val_accuracy_sum / len(valid_loader)).cpu()
+          val_loss = (val_loss_sum / len(valid_loader)).cpu()
+
+          # Store the values in the dictionary
+
+          # Print to console
+          print(f"EPOCH = {epoch} --- ITERATION = {iteration}")
+          print(f"Validation loss = {val_loss} --- Validation accuracy = {val_accuracy}")
       #logging results
-    print(f"loss: {train_loss.item()} epoch: {epoch}")
+    training_loss = train_loss.cpu()
+    losses[epoch] = training_loss
+    print(f"\n\nloss: {training_loss.item()} epoch: {epoch}")
     print("It has now been "+ time.strftime("%Mm%Ss", time.gmtime(time.time() - start))  +"  since the beginning of the program")
     text_file = open(r"results\training\losses.txt", "a")  
-    text_file.write(f"loss: {train_loss.item()} epoch: {epoch}\n")
+    text_file.write(f"loss: {training_loss.item()} epoch: {epoch}\n")
     current =  time.strftime("%Hh%Mm%Ss", time.gmtime(time.time() - start))
     text_file.write(f"It has now been {current} since the beginning of the program\n")
-      
-  return cnn
+    text_file.close()
+
+    
+
+  text_file = open(r"results\training\losses.txt", "a") 
+  text_file.write(f"{losses}")
+  text_file.close()
+  return losses
 
 
 if __name__ == "__main__":
-  train_loader, valid_loader, test_loader = ld.load_data_mnist()
-  cnn = CNN_model(numberConvolutionLayers=3,initialKernels=5,numberDense=0,neuronsDLayer=100,dropout=0.5, dataset="MNIST")
-  summary(cnn, (1, 28, 28))
-  cnn = train_model(cnn, train_loader, valid_loader, test_loader,num_epochs=100)
+  number_of_epochs = 500
+  train_loader, valid_loader, test_loader = ld.load_data()
+  cnn = CNN_model(numberConvolutionLayers=4,initialKernels=64,numberDense=0,neuronsDLayer=1024,dropout=0.5, dataset="ASL").to(DEVICE)
+  summary(cnn, (3, 32, 32))
+  losses = train_model(cnn, train_loader, valid_loader, test_loader,num_epochs=number_of_epochs)
+  xh = np.arange(0,number_of_epochs)
+  plt.plot(xh, losses, color = 'b', 
+         marker = ',',label = "Loss") 
+  plt.xlabel("Epochs Traversed")
+  plt.ylabel("Training Loss")
+  plt.grid() 
+  plt.legend() 
+  plt.show()
--- src/loading_dataset.py
+++ src/loading_dataset.py
@@ -6,6 +6,7 @@ import opendatasets as od
 import matplotlib.pyplot as plt
 import numpy as np
 import torchvision
+from hyperpara_tuning import find_mean_stds
 
 PATH_TO_DATA = r"synthetic-asl-alphabet"
 test_data_path = PATH_TO_DATA + r"/Test_Alphabet"
@@ -27,7 +28,7 @@ def load_data_mnist():
 
     # Preparing the training, validation, and test sets
     train_and_val_sets = torchvision.datasets.MNIST('mnist', train=True, download=True, transform=transform) # Will be split into training and validation for hyperparameter tuning
-    train_set, val_set = torch.utils.data.random_split(train_and_val_sets, [55000, 5000]) # 55000 training examples, 5000 validation examples
+    train_set, val_set,_ = torch.utils.data.random_split(train_and_val_sets, [5000, 1000,54000]) # 55000 training examples, 5000 validation examples
     test_set = torchvision.datasets.MNIST('mnist', train=False, download=True, transform=transform)
 
     # Prepare loaders which will give us the data batch by batch (Here, batch size is 8)
@@ -51,19 +52,27 @@ def load_data():
     # means = [0.4916, 0.4699, 0.4255] # after hyper para tuning gives very intense pics
     # standard_deviations = [0.0251, 0.0265, 0.0278]
     
-    means = [0.2,0.2,0.2] # gives descent pics but pick black for dark background with dark skin tones
-    standard_deviations = [0.8,0.8,0.8]
+    train_means = [0.4904, 0.4686, 0.4246] # gives descent pics but pick black for dark background with dark skin tones
+    train_standard_deviations = [0.2473, 0.2526, 0.2713]
+
+    test_means = [0.4904, 0.4686, 0.4246]
+
+    test_standard_deviations = [0.2473, 0.2526, 0.2713]
     
     # Define the transformations, including normalization
-    processing_transforms = transforms.Compose([
+    train_processing_transforms = transforms.Compose([
+        transforms.ToTensor(),  # Convert PIL Image to tensor
+        # transforms.Grayscale(num_output_channels=3),
+        transforms.Resize((32,32)),
+        
+    ])
+    test_processing_transforms = transforms.Compose([
         transforms.ToTensor(),                             # Convert PIL Image to tensor
-        transforms.Normalize(means, standard_deviations),   # Normalize the image
-        transforms.RandomRotation(15),                      # Data augmentation
-        transforms.RandomHorizontalFlip()
+        transforms.Normalize(test_means, test_standard_deviations),   # Normalize the image
     ])
     
-    full_train_dataset = datasets.ImageFolder(train_data_path,transform=processing_transforms)
-    test_dataset = datasets.ImageFolder(test_data_path,transform=processing_transforms)
+    full_train_dataset = datasets.ImageFolder(train_data_path,transform=train_processing_transforms)
+    test_dataset = datasets.ImageFolder(test_data_path,transform=test_processing_transforms)
 
     
     # # Loading The Dataset
@@ -71,13 +80,14 @@ def load_data():
     # test_dataset = datasets.ImageFolder(test_data_path, transform=transforms.Compose(processing_transforms))
     
     # Split the datasets into training, validation, and test sets
-    train_dataset, valid_dataset, _ = random_split(full_train_dataset, [int(TRAIN_SET_SIZE*0.25), int(TRAIN_SET_SIZE*0.25),int(TRAIN_SET_SIZE*0.50)])
+    train_dataset, valid_dataset, _ = random_split(full_train_dataset, [int(TRAIN_SET_SIZE*0.01), int(TRAIN_SET_SIZE*0.01),int(TRAIN_SET_SIZE*0.98)])
     test_dataset, _, _ = random_split(test_dataset, [TESTING_SET_SIZE, len(test_dataset) - TESTING_SET_SIZE, 0])
     
+
     # Set Batch Size and shuffles the data
-    train_loader = DataLoader(train_dataset, batch_size=10, shuffle=True)
-    valid_loader = DataLoader(valid_dataset, batch_size=10, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=10, shuffle=True)
+    train_loader = DataLoader(train_dataset, batch_size=3, shuffle=True)
+    valid_loader = DataLoader(valid_dataset, batch_size=3, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=3, shuffle=True)
     
     print(f"Training set size: {len(train_dataset)}")
     print(f"Validation set size: {len(valid_dataset)}")
@@ -107,6 +117,7 @@ if __name__ == "__main__":
     
     for images, labels in train_loader:
         if counter == 5 : break
+        # print(images[counter])
         plt.imshow(images[counter].permute(1, 2, 0))
         plt.title(f"Label: {labels[0]}")
         plt.show()
